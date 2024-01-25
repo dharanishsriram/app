42368afa66ee52711bee68a718f203ca01c2427a
dharanishsriram <dharanishsriraml.22bir@kongu.edu>
2024-01-25 16:30:30 +0530

    version2

---
+++ src/Service.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Service.css'
+export default function() {
+  return (
+    <div>
+        <div className='service'>
+            <div className='service-item'>01</div>
+            <div className='service-item'>02</div>
+            <div className='service-item'>03</div>
+            <div className='service-item'>04</div>
+        </div>
+    </div>
+  )
+}
